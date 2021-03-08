8fcc3a677ce1a86aa7bfa138dc801b3d7b6c8b1e
Ade Fajr Ariav <iav@Ades-MacBook-Pro.local>
2021-03-08 11:54:06 +0700

    fixing camera front

--- app/src/main/java/com/pdamkotasmg/happywork/fitur/absensi/AbsensiActivity.java
+++ app/src/main/java/com/pdamkotasmg/happywork/fitur/absensi/AbsensiActivity.java
@@ -2,8 +2,11 @@ package com.pdamkotasmg.happywork.fitur.absensi;
 
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,47 +15,55 @@ import com.pdamkotasmg.happywork.R;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class AbsensiActivity extends AppCompatActivity {
 
-    private CameraKitView camera;
-    private Button btnJepret;
-
     private int currentTime;
-    private String  currentDate;
-    private String  nameUser;
+    private String currentDate;
+    private String nameUser;
+
+    private CameraKitView cameraBack;
+    private CameraKitView cameraFront;
+    private Button btnJepret;
+    private LinearLayout divCamera;
+    private ImageView ivHeaderBackArrow;
+    private TextView tvHeaderJudul;
+    private ImageView ivHeaderInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_absensi);
+        getSupportActionBar().hide();
         initView();
         nameUser = "Nama_";
+        tvHeaderJudul.setText("REKAM RUPAMU");
+
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
         currentTime = calendar.get(Calendar.MILLISECOND);
         Date cDate = new Date();
         currentDate = new SimpleDateFormat("yyyy-MM-dd").format(cDate);
-        camera.startVideo();
-        btnJepret.setOnClickListener(v -> {
-            camera.captureImage(new CameraKitView.ImageCallback() {
-                @Override
-                public void onImage(CameraKitView cameraKitView, byte[] bytes) {
-                    File makeFile = new File(Environment.getExternalStorageDirectory() + "/PDAM-ABSENSI");
-                    makeFile.mkdirs();
-                    File savedPhoto = new File(makeFile, nameUser + currentDate + "_" + currentTime + "_FF.xxkampretnotfailable");
-                    try {
-                        FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
-                        Log.d("debug", "onImage: " + savedPhoto);
-                        Log.d("debug", "outStram: " + outputStream);
-                        outputStream.write(bytes);
-                        outputStream.close();
-                    } catch (java.io.IOException e) {
-                        e.printStackTrace();
-                    }
+
+        cameraBack.setVisibility(View.GONE);
+        cameraFront.startVideo();
+        divCamera.setOnClickListener(v -> {
+            cameraFront.captureImage((cameraKitView, bytes) -> {
+                File makeFile = new File(Environment.getExternalStorageDirectory() + "/PDAM-ABSENSI");
+                makeFile.mkdirs();
+                File savedPhoto = new File(makeFile, nameUser + currentDate + "_" + currentTime + "_FF.xxkampretnotfailable");
+                try {
+                    FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
+                    outputStream.write(bytes);
+                    outputStream.close();
+                    cameraFront.onStop();
+                    cameraFront.stopVideo();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             });
         });
@@ -60,36 +71,40 @@ public class AbsensiActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
+        cameraFront.onStart();
         super.onStart();
-        camera.onStart();
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        camera.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        camera.onPause();
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        camera.onStop();
-        super.onStop();
-    }
+//
+//    @Override
+//    protected void onPause() {
+//        cameraFront.onPause();
+//        super.onPause();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        cameraFront.onStop();
+//        super.onStop();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        cameraFront.onStart();
+//        super.onResume();
+//    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        camera.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraFront.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     private void initView() {
-        camera = findViewById(R.id.camera);
-        btnJepret = findViewById(R.id.btn_jepret);
+        cameraBack = findViewById(R.id.camera_back);
+        cameraFront = findViewById(R.id.camera_front);
+        divCamera = findViewById(R.id.div_camera);
+        ivHeaderBackArrow = findViewById(R.id.iv_header_back_arrow);
+        tvHeaderJudul = findViewById(R.id.tv_header_judul);
+        ivHeaderInfo = findViewById(R.id.iv_header_info);
     }
 }
