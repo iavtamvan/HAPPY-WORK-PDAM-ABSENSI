81444b48911ece1d0e5636bee9594323bd59b4f0
iavtamvan <ade.fajr.ariav@gmail.com>
2021-03-16 14:54:14 +0700

    Add intent to dashboard

--- app/src/main/java/com/pdamkotasmg/happywork/fitur/authentication/login/LoginActivity.java
+++ app/src/main/java/com/pdamkotasmg/happywork/fitur/authentication/login/LoginActivity.java
@@ -1,17 +1,31 @@
 package com.pdamkotasmg.happywork.fitur.authentication.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.pdamkotasmg.happywork.R;
+import com.pdamkotasmg.happywork.fitur.dashboard.DashboardActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnMasuk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+        initView();
+        btnMasuk.setOnClickListener(v -> {
+            finishAffinity();
+            startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+        });
+    }
+
+    private void initView() {
+        btnMasuk = findViewById(R.id.btn_masuk);
     }
 }
