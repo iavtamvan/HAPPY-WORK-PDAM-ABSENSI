f39dd54fec58c548f1a0141b411673788b1a2ef5
iavtamvan <ade.fajr.ariav@gmail.com>
2022-01-07 11:51:51 +0700

    adding ACTIVITY didalam Menu Kehadiran

---
+++ app/src/main/java/com/pdamkotasmg/goodday/fitur/kehadiran/view/PerjalananDinasActivity.java
@@ -0,0 +1,16 @@
+package com.pdamkotasmg.goodday.fitur.kehadiran.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.pdamkotasmg.goodday.R;
+
+public class PerjalananDinasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perjalanan_dinas);
+    }
+}
