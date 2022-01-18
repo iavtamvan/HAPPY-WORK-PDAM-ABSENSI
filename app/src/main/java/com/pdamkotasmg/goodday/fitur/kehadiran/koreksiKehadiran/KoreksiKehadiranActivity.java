180ea714b45c2267ccd15d23dc3d613d8d25a5e7
iavtamvan <ade.fajr.ariav@gmail.com>
2022-01-18 10:19:01 +0700

    Koreksi kehadiran => proses kehadiran

--- app/src/main/java/com/pdamkotasmg/goodday/fitur/kehadiran/koreksiKehadiran/KoreksiKehadiranActivity.java
+++ app/src/main/java/com/pdamkotasmg/goodday/fitur/kehadiran/koreksiKehadiran/KoreksiKehadiranActivity.java
@@ -1,8 +1,10 @@
 package com.pdamkotasmg.goodday.fitur.kehadiran.koreksiKehadiran;
 
+import android.app.ProgressDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -10,19 +12,29 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.ads.AdView;
 import com.google.gson.Gson;
+import com.krishna.securetimer.SecureTimer;
 import com.pdamkotasmg.goodday.R;
 import com.pdamkotasmg.goodday.api.server.ApiConfig;
 import com.pdamkotasmg.goodday.api.server.ApiService;
+import com.pdamkotasmg.goodday.fitur.kehadiran.home.adapter.KehadiranAdapter;
+import com.pdamkotasmg.goodday.fitur.kehadiran.home.model.DataItem;
+import com.pdamkotasmg.goodday.fitur.kehadiran.home.model.RiwayatKehadiranRootModel;
 import com.pdamkotasmg.goodday.fitur.kehadiran.koreksiKehadiran.model.postKoreksiKehadiran.DetailsItem;
 import com.pdamkotasmg.goodday.fitur.kehadiran.koreksiKehadiran.model.postKoreksiKehadiran.KoreksiKeharidanRootModel;
 import com.pdamkotasmg.goodday.utils.Config;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -38,10 +50,14 @@ public class KoreksiKehadiranActivity extends AppCompatActivity implements DateP
     private String name;
     private String npp;
     private ArrayList<DetailsItem> listDetails = new ArrayList<>();
+    private KehadiranAdapter kehadiranAdapter;
+    private List<DataItem> dataItems;
 
     private String flag;
     private String startDate;
     private String endDate;
+    private SimpleDateFormat formatDate;
+    private Date currentTimeInMillis;
 
     private ImageView ivHeaderBackArrow;
     private TextView tvHeaderJudul;
@@ -51,6 +67,11 @@ public class KoreksiKehadiranActivity extends AppCompatActivity implements DateP
     private EditText edtEndDate;
     private AdView adView;
     private Button btnNewRequest;
+    private RecyclerView rvDetailsAttedance;
+    private TextView tvListKoreksiKehadiranRequestAt;
+    private EditText edtReason;
+    private EditText edtStartTime;
+    private EditText edtEndTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +85,9 @@ public class KoreksiKehadiranActivity extends AppCompatActivity implements DateP
         name = sharedPreferences.getString(Config.SHARED_NAME, "");
         npp = sharedPreferences.getString(Config.SHARED_NPP_PROFILE, "");
         Log.d(TAG, "token: " + accesToken);
+
+        currentTimeInMillis = SecureTimer.with(KoreksiKehadiranActivity.this).getCurrentDate();
+
 
         edtRequestFor.setText(name + " (" + npp + ")");
         edtRequestFor.setOnClickListener(v -> {
@@ -83,7 +107,7 @@ public class KoreksiKehadiranActivity extends AppCompatActivity implements DateP
             dpd.setThemeDark(true);
             dpd.show(getSupportFragmentManager(), "Datepickerdialog");
         });
-        
+
         edtEndDate.setOnClickListener(v -> {
             flag = "endDate";
             Calendar now = Calendar.getInstance();
@@ -103,6 +127,42 @@ public class KoreksiKehadiranActivity extends AppCompatActivity implements DateP
         });
 
 //        postJsonKoreksiKehadiran();
+    }
+
+    private void getHistoryPresensi() {
+        ProgressDialog progressDialog = new ProgressDialog(KoreksiKehadiranActivity.this);
+        progressDialog.setMessage("Mohon tunggu...");
+        progressDialog.show();
+        ApiService apiService = ApiConfig.getApiService();
+        apiService.getHistoryPresensi(accesToken, edtStartDate.getText().toString().trim(), edtEndDate.getText().toString().trim(), "1") // tanggal dari, dan tanggal selesai
+                .enqueue(new Callback<RiwayatKehadiranRootModel>() {
+                    @Override
+                    public void onResponse(Call<RiwayatKehadiranRootModel> call, Response<RiwayatKehadiranRootModel> response) {
+                        if (response.isSuccessful()) {
+                            progressDialog.cancel();
+                            dataItems = new ArrayList<>();
+                            dataItems = response.body().getData();
+                            rvDetailsAttedance.setVisibility(View.VISIBLE);
+                            if (dataItems.isEmpty()) {
+                                Toast.makeText(KoreksiKehadiranActivity.this, "Data tidak ada", Toast.LENGTH_SHORT).show();
+                            } else {
+                                kehadiranAdapter = new KehadiranAdapter(KoreksiKehadiranActivity.this, dataItems);
+                                rvDetailsAttedance.setLayoutManager(new LinearLayoutManager(KoreksiKehadiranActivity.this));
+                                rvDetailsAttedance.setAdapter(kehadiranAdapter);
+                                kehadiranAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            progressDialog.cancel();
+                            Toast.makeText(KoreksiKehadiranActivity.this, "" + response.message(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<RiwayatKehadiranRootModel> call, Throwable t) {
+                        progressDialog.cancel();
+                        Toast.makeText(KoreksiKehadiranActivity.this, "" + Config.ERROR_MSG, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     private void postJsonKoreksiKehadiran() {
@@ -149,6 +209,11 @@ public class KoreksiKehadiranActivity extends AppCompatActivity implements DateP
         edtEndDate = findViewById(R.id.edt_end_date);
         adView = findViewById(R.id.adView);
         btnNewRequest = findViewById(R.id.btn_new_request);
+        rvDetailsAttedance = findViewById(R.id.rv_details_attedance);
+        tvListKoreksiKehadiranRequestAt = findViewById(R.id.tv_list_koreksi_kehadiran_request_at);
+        edtReason = findViewById(R.id.edt_reason);
+        edtStartTime = findViewById(R.id.edt_start_time);
+        edtEndTime = findViewById(R.id.edt_end_time);
     }
 
     @Override
@@ -157,11 +222,27 @@ public class KoreksiKehadiranActivity extends AppCompatActivity implements DateP
 //            String date = "You picked the following date: " + dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
             String date = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
             startDate = date;
-            edtStartDate.setText(startDate);
+            try {
+                SimpleDateFormat fmt = new SimpleDateFormat("yyyy-mm-dd");
+                Date dates = fmt.parse(startDate);
+                String dateFinal = new SimpleDateFormat("yyyy-MM-dd").format(dates);
+                edtStartDate.setText(dateFinal);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
         } else {
             String date = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
             endDate = date;
-            edtEndDate.setText(endDate);
+            try {
+                SimpleDateFormat fmt = new SimpleDateFormat("yyyy-mm-dd");
+                Date dates = fmt.parse(endDate);
+                String dateFinal = new SimpleDateFormat("yyyy-MM-dd").format(dates);
+                edtEndDate.setText(dateFinal);
+                getHistoryPresensi();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         }
     }
 
