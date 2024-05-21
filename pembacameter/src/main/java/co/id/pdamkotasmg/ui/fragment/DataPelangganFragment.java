ed6ad94b05535db47e5ee182fdb8101127d7f000
iH <ade.fajr.ariav@gmail.com>
2024-05-21 11:36:09 +0700

    adding report&laporan

--- pembacameter/src/main/java/co/id/pdamkotasmg/ui/fragment/DataPelangganFragment.java
+++ pembacameter/src/main/java/co/id/pdamkotasmg/ui/fragment/DataPelangganFragment.java
@@ -1,10 +1,13 @@
 package co.id.pdamkotasmg.ui.fragment;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,15 +17,48 @@ import co.id.pdamkotasmg.pembacameter.databinding.FragmentDataPelangganBinding;
 public class DataPelangganFragment extends Fragment {
 
     private FragmentDataPelangganBinding binding;
+    private ProgressDialog progressDialog;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentDataPelangganBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
 
-        final TextView textView = binding.textNotifications;
+        WebSettings webSettings = binding.webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Set a WebViewClient to handle navigation events
+        binding.webView.setWebViewClient(new WebViewClient());
+
+        // Set a WebViewClient to handle navigation events
+        binding.webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, android.graphics.Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressDialog.show();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressDialog.dismiss();
+            }
+        });
+
+        // Load the URL with POST data
+        loadUrlWithPostData("https://app.pdamkotasmg.co.id/be-proxy/siramhpm/login/login-02-proses-api.php",
+                "user=2002&pass=2002");
+
         return root;
+    }
+
+    private void loadUrlWithPostData(String url, String postData) {
+        byte[] postDataBytes = postData.getBytes();
+        binding.webView.postUrl(url, postDataBytes);
     }
 
     @Override
