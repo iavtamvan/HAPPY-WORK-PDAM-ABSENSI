67d093cc53a122675455a20c42620fe7264bd0c1
iH <ade.fajr.ariav@gmail.com>
2025-01-03 15:19:13 +0700

    build ui manometer

---
+++ pembacameter/src/main/java/co/id/pdamkotasmg/ui/activity/SingleManometerActivity.java
@@ -0,0 +1,26 @@
+package co.id.pdamkotasmg.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import co.id.pdamkotasmg.pembacameter.R;
+
+public class SingleManometerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_single_manometer);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
